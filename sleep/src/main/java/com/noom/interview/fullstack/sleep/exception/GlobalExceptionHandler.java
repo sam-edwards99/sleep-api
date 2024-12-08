f85fe36fbbf005f61bfe1ecac71475b347bee1ce
Sam Edwards <samedwards@mac.mynetworksettings.com>
2024-12-07 19:03:48 -0500

    Add global exception handler

---
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.noom.interview.fullstack.sleep.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // Handle Validation Failures
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), errors.values().toString()), HttpStatus.BAD_REQUEST);
+    }
+}
